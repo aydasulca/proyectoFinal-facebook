964bceafbf2ae4e9f640e037a24c1a9920a55768
Ayda Sulca <suni_yuri_94@hotmail.com>
2017-03-10 13:26:19 -0500

    Muro

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -4,20 +4,22 @@ window.addEventListener('load',function() {
     var email= document.getElementById('email').value;
     var password = document.getElementById('password').value;
     var validar_email = document.getElementById('validar_email');
-    if(email != ""){
-      if (email != /\S+@\S+\.\S+/ && email !=) {
-
+    var validar_password = document.getElementById('validar_password');
+    var correo = /\S+@\S+\.\S+/;
+    var datos_usuario= ["dagui_22@hotmail.com", "ana_undefined@hotmail.com", "geral_26@hotmail.com"];
+    if(email !=""){
+    if(correo.test(email) != true){
+      validar_email.innerHTML = "Este correo no es valido";
+    }
+    }else {
+      validar_email.innerHTML = "El campo de usuario no puede estar en blanco";
+    };
+    if(password == ""){
+      validar_password.innerHTML = "Complete el campo de password";
+    }
+    for (var i=0; i < datos_usuario.length; i++){
+      if(email == datos_usuario){
       }
-
-      }else {
-        validar_email.innerHTML = "El campo de usuario no puede estar en blanco";
-      };
-      if (password != "") {
-
-      }else {
-        validar_password.innerHTML = "El campo de password no puede estar en blanco";
-      }
-
-
-  });
+    };
+ });
 });
